fbbe880bd7ed3067b8a3cf241eaf90b30985022f
Moisés Guimarães <moises@wolfssl.com>
2018-01-04 23:50:50 -0300

    fixes travis python installation

--- make/osx/install_python.sh
+++ make/osx/install_python.sh
@@ -5,8 +5,8 @@ set -x
 URLS=(
     'https://www.python.org/ftp/python/2.7.14/python-2.7.14-macosx10.6.pkg'
     'https://www.python.org/ftp/python/3.4.4/python-3.4.4-macosx10.6.pkg'
-    'https://www.python.org/ftp/python/3.5.3/python-3.5.4-macosx10.6.pkg'
-    'https://www.python.org/ftp/python/3.6.0/python-3.6.4-macosx10.6.pkg'
+    'https://www.python.org/ftp/python/3.5.4/python-3.5.4-macosx10.6.pkg'
+    'https://www.python.org/ftp/python/3.6.4/python-3.6.4-macosx10.6.pkg'
 )
 
 for url in "${URLS[@]}"; do
