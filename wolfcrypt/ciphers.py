e65ce4288ac75d270860fe0f4143b0e186bea75f
Eric Blankenhorn <eric@wolfssl.com>
2023-12-26 16:32:20 -0600

    Exclude AES CTR from block size check

--- wolfcrypt/ciphers.py
+++ wolfcrypt/ciphers.py
@@ -181,7 +181,7 @@ class _Cipher(object):
             raise ValueError(
                     "empty string not allowed")
 
-        if len(string) % self.block_size and not "ChaCha" in self._native_type:
+        if len(string) % self.block_size and not self.mode == MODE_CTR and not "ChaCha" in self._native_type:
             raise ValueError(
                 "string must be a multiple of %d in length" % self.block_size)
 
@@ -214,7 +214,7 @@ class _Cipher(object):
             raise ValueError(
                     "empty string not allowed")
 
-        if len(string) % self.block_size and not "ChaCha" in self._native_type:
+        if len(string) % self.block_size and not self.mode == MODE_CTR and not "ChaCha" in self._native_type:
             raise ValueError(
                 "string must be a multiple of %d in length" % self.block_size)
 
